16e31a66d8c9722b1bea8b2d27b19bf1793eb1e9
João Victor Oliveira <joao.oliveira@pitang.com>
2020-01-27 09:45:55 -0300

    update user

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -10,6 +10,30 @@ class UserController {
     const { id, name, email, instructor } = await User.create(req.body);
     return res.json({ id, name, email, instructor });
   }
+
+  async update(req, res) {
+    const { email, oldPassword } = req.body;
+
+    const user = await User.findByPk(req.userId);
+
+    if (email !== user.email) {
+      const userExists = await User.findOne({
+        where: { email },
+      });
+
+      if (userExists) {
+        return res.status(400).json({ error: 'Email already exists' });
+      }
+    }
+
+    if (oldPassword && !(await user.checkPassword(oldPassword))) {
+      return res.status(401).json({ error: 'Password does not match' });
+    }
+
+    const { id, name, instructor } = await user.update(req.body);
+
+    return res.json({ id, name, email, instructor });
+  }
 }
 
 export default new UserController();
